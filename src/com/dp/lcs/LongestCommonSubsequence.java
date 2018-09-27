1ba155fae257049f977d652856283096226d32fd
Mona Lisa <yogesh.kumar@techniatranscat.com>
2018-09-27 23:41:21 +0530

    improve iteration

--- src/com/dp/lcs/LongestCommonSubsequence.java
+++ src/com/dp/lcs/LongestCommonSubsequence.java
@@ -4,6 +4,15 @@ public class LongestCommonSubsequence {
 
 	public static void main(String[] args) {
 
+		LongestCommonSubsequence lc = new LongestCommonSubsequence();
+		String s1 = "AGGTAB";
+		String s2 = "GXTXAYB";
+
+		char[] X = s1.toCharArray();
+		char[] Y = s2.toCharArray();
+		int m = X.length;
+		int n = Y.length;
+		System.out.println(lc.lcsdp(X, Y, m, n));
 	}
 
 	public int lcs(char[] a, char[] b, int m, int n) {
@@ -25,12 +34,12 @@ public class LongestCommonSubsequence {
 	public int lcsdp(char[] a, char[] b, int m, int n) {
 
 		int[][] l = new int[m + 1][n + 1];
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
+		for (int i = 0; i <= m; i++) {
+			for (int j = 0; j <= n; j++) {
 				if (i == 0 || j == 0) {
 					l[i][j] = 0;
 				}
-				if (a[i - 1] == b[j - 1]) {
+				else if (a[i - 1] == b[j - 1]) {
 					l[i][j] = l[i - 1][j - 1] + 1;
 				} else {
 					l[i][j] = max(l[i - 1][j], l[i][j - 1]);
