4a9b1ce005de0ecf40562ba8b3e5fdbea21ddb98
Mona Lisa <yogesh.kumar@techniatranscat.com>
2018-10-01 12:17:57 +0530

    very small modification

--- src/com/dp/lcs/KnapSack.java
+++ src/com/dp/lcs/KnapSack.java
@@ -22,9 +22,9 @@ public class KnapSack {
 				if (i == 0 || w == 0) {
 					dp[i][j] = 0;
 				} else if (weights[j] > w) {
-					dp[i][j] = dp[i-1][j];
+					dp[i][j] = dp[i - 1][j];
 				} else {
-					dp[i][j] = max(values[j] + dp[i][j-weights[i-1]], dp[i-1][j]);
+					dp[i][j] = max(values[j] + dp[i][j - weights[i - 1 ]], dp[i - 1][j]);
 				}
 			}
 		}
